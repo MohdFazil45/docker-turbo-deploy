52835608823572f5c1b643c628085233bb897598
MohdFazil45 <mdfazil846@gmail.com>
2026-02-28 07:08:56 +0530

    added basic turbo package

---
+++ apps/httpBackend/index.ts
@@ -0,0 +1,49 @@
+import "dotenv/config"
+import express, { type Request, type Response } from "express";
+import { prisma } from "@repo/db/client"
+const app = express()
+
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany()
+
+  if (!users) {
+    return res.status(404).json({
+      error:"User not exist"
+    })
+  }
+
+  res.status(200).json({
+    users
+  })
+   
+})
+
+app.post("/user", async   (req, res) => {
+  const { username, password } = req.body;
+  
+  if (!username || !password) {
+    res.status(400).json({ error: "Username and password are required" });
+    return
+  }
+
+  await prisma.user.create({
+    data: {
+      username,
+      password
+    }
+  })
+    .then(users => {
+      res.status(201).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+})
+
+
+app.listen(3000,()=> {
+    console.log("Server started")
+})
---
+++ apps/wsBackend/index.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@repo/db/client";
+
+Bun.serve({
+  port: 8081,
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    return new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {
+    async message(ws, message) {
+      try {
+        await prisma.user.create({
+          data: {
+            username: Math.random().toString(),
+            password: Math.random().toString(),
+          },
+        });
+        ws.send(message);
+      } catch (err) {
+        console.error("DB error:", err);
+        ws.send("Error creating user");
+      }
+    },
+  },
+});
